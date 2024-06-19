e72b5dc32d6a974e70030138dd115e7c0de9d7e7
Gabriel tomiatti <gabrieltomiatti9@gmail.com>
2024-06-18 22:24:31 -0300

    rust hello

---
+++ ferris-Fale/target/debug/deps/ferris_Fale-37db9fed530dff52.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/Frosty/Documents/Rust study/rust-ferris/ferris-Fale/target/debug/deps/ferris_Fale-37db9fed530dff52: src/main.rs
+
+/mnt/c/Users/Frosty/Documents/Rust study/rust-ferris/ferris-Fale/target/debug/deps/ferris_Fale-37db9fed530dff52.d: src/main.rs
+
+src/main.rs:
---
+++ ferris-Fale/target/debug/ferris-Fale.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/Frosty/Documents/Rust\ study/rust-ferris/ferris-Fale/target/debug/ferris-Fale: /mnt/c/Users/Frosty/Documents/Rust\ study/rust-ferris/ferris-Fale/src/main.rs
